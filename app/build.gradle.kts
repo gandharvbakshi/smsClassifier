eb478bc62c5c7f7e28494322c7df3c39978529aa
gandharvbakshi <gandharv.bakshi@gmail.com>
2026-06-21 07:35:14 +0530

    Polish Pro trial onboarding copy

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,8 +28,8 @@ android {
         applicationId = "com.smsclassifier.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 37
-        versionName = "1.2.12"
+        versionCode = 38
+        versionName = "1.2.13"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
