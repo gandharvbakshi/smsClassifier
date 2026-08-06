e735c0edc8485e8c4ba44cba2e47d14c84c00bf2
gandharvbakshi <gandharv.bakshi@gmail.com>
2026-08-06 09:28:59 +0530

    Prepare 1.2.31 Open Testing release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,8 +32,8 @@ android {
         applicationId = "com.smsclassifier.app"
         minSdk = 26
         targetSdk = 36
-        versionCode = 55
-        versionName = "1.2.30"
+        versionCode = 56
+        versionName = "1.2.31"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
